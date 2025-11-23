d09b1e569917f79b9e87bb373b0959adb602d498
hangyeol <hwa2367@gmail.com>
2025-11-23 13:32:18 +0900

    메인 및 패널 부분 제외
    feature/client/GUI/dashboard_GUI.java

--- PROGRAM_Client/src/client/java/program_name/GUI/DashBoard_Gui.java
+++ PROGRAM_Client/src/client/java/program_name/GUI/DashBoard_Gui.java
@@ -22,29 +22,6 @@ public class DashBoard_Gui extends JFrame {
     JTextField tfSearch;
     DefaultTableModel table;
 
-    public DashBoard_Gui () {
-        super("대시보드 구현");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000, 640);
-        setLocationRelativeTo(null);
-        setLayout(new BorderLayout());
-    
-        JTabbedPane tabs = new JTabbedPane();
-        tabs.addTab("대시보드", buildDashboardPanel("대시보드"));
-        // tabs.addTab("동작 등록", buildEmptyPanel("동작 등록"));
-        // tabs.addTab("가이드", buildEmptyPanel("가이드")); 
-        // tabs.addTab("목표 달성 트래커", buildEmptyPanel("목표 달성 트래커"));
-        // tabs.addTab("도감", buildEmptyPanel("도감"));
-
-        add(tabs, BorderLayout.CENTER);
-    }
-
-    // 메인 함수
-    public static void main(String[] args) {
-        // Swing 애플리케이션 시작
-        SwingUtilities.invokeLater(() -> new test().setVisible(true));
-    }
-
     private JPanel buildDashboardPanel(String title) {
         JPanel p = new JPanel(new BorderLayout());
         p.setBorder(new EmptyBorder(12, 12, 12, 12));
