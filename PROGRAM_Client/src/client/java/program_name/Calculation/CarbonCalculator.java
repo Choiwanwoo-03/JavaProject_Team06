365ecd4c873caf329bdeac113f8c1463b8f37f15
이한초아 <goeasyoncho@gmail.com>
2025-11-25 13:11:16 +0900

    PROGRAM/src/client/java/program_name/Calculation/CarbonCalculator.java
    · AddAction_Gui.java 클래스의 탄소 배출량 계산 코드를 CarbonCalculator.java 클래스로 이동

--- PROGRAM_Client/src/client/java/program_name/Calculation/CarbonCalculator.java
+++ PROGRAM_Client/src/client/java/program_name/Calculation/CarbonCalculator.java
@@ -1,5 +1,27 @@
 package Calculation;
 
-public class CarbonCalculator {
+import java.util.Map;
 
+public class CarbonCalculator {
+	private final Map<String, Double> co2eFactors;
+	
+	public CarbonCalculator(Map<String, Double> co2eFactors) {
+		this.co2eFactors = co2eFactors;
+	}
+	
+	public double CalculateActionCarbon(String actionName, double count, String tabTitle) {
+		double factor = co2eFactors.getOrDefault(actionName, 0.0);
+		
+		if (factor == 0.0) {
+			return 0.0;
+		}
+		
+		double adCount = count;
+		
+		if ("쓰레기".equals(tabTitle)) {
+			adCount = count / 100.0;
+		}
+		
+		return adCount * factor;
+	}
 }
