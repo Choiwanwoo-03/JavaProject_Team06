da51b9c1a413cba46b61ff35d68bf17799a0fb6c
wanu_c <chldhksdn37@gmail.com>
2025-11-29 01:40:49 +0900

    feat : ServerSocket의 객체 생성 후 소캣 연결에서 문제 발생 해결 위치 : 브랜치) feature / ServerSocket / wanwoo 폴더) server / Socket / ServerSocket.java

--- PROGRAM_Server/src/server/java/program_name/Socket/ServerSocket.java
+++ PROGRAM_Server/src/server/java/program_name/Socket/ServerSocket.java
@@ -1,33 +1,29 @@
 package Socket;
 
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class ServerSocket {
 
-    private ServerSocket server;  // 포트 바인딩용
-    private final int PORT = 5000;  // 원하는 포트 번호
+    private java.net.ServerSocket serverSocket;  // ← 이름 충돌 해결
+    private final int PORT = 5000;
     public static ArrayList<ConnectedClient> clients = new ArrayList<>();
 
-    // 서버 시작 메소드
     public void startServer() {
         try {
-            server = new ServerSocket(PORT);
+            serverSocket = new java.net.ServerSocket(PORT); // ← 충돌 해결
             System.out.println("[SERVER] 포트 " + PORT + " 에서 서버가 시작되었습니다!");
 
             while (true) {
                 System.out.println("[SERVER] 클라이언트 접속 대기중...");
-                Socket clientSock = server.accept();  
+                Socket clientSock = serverSocket.accept();
 
                 System.out.println("[SERVER] 클라이언트 연결됨: " + clientSock.getInetAddress());
 
-                // 클라이언트 객체 생성
                 ConnectedClient client = new ConnectedClient(clientSock, this);
                 clients.add(client);
 
-                // 스레드로 실행
                 client.start();
             }
 
@@ -36,7 +32,6 @@ public class ServerSocket {
         }
     }
 
-    // 클라이언트 리스트에서 제거 (종료 시 호출)
     public void removeClient(ConnectedClient client) {
         clients.remove(client);
     }
